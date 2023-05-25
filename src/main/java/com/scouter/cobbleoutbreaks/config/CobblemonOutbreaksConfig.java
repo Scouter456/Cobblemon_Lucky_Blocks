d5b35fca58d8ca4eff02b5736af0a41614b2be0d
Scouter456 <markveenstra14@gmail.com>
2023-05-25 20:56:02 +0200

    Added biome specific spawning for pokemon

--- src/main/java/com/scouter/cobbleoutbreaks/config/CobblemonOutbreaksConfig.java
+++ src/main/java/com/scouter/cobbleoutbreaks/config/CobblemonOutbreaksConfig.java
@@ -18,6 +18,11 @@ public class CobblemonOutbreaksConfig {
     public static ForgeConfigSpec.ConfigValue<Integer> OUTBREAK_SPAWN_COUNT;
     public static ForgeConfigSpec.ConfigValue<Boolean> SEND_PORTAL_SPAWN_MESSAGE;
     public static ForgeConfigSpec.ConfigValue<Boolean> SPAWN_PORTAL_PARTICLES;
+    public static ForgeConfigSpec.ConfigValue<Boolean> BIOME_SPECIFIC_SPAWNS;
+    public static ForgeConfigSpec.ConfigValue<Boolean> BIOME_SPECIFIC_SPAWNS_DEBUG;
+
+    public static ForgeConfigSpec.ConfigValue<Integer> MIN_SPAWN_RADIUS;
+    public static ForgeConfigSpec.ConfigValue<Integer> MAX_SPAWN_RADIUS;
     private static void setupConfig(ForgeConfigSpec.Builder builder) {
         builder.comment(CobblemonOutbreaks.MODID + " Config");
 
@@ -26,6 +31,10 @@ public class CobblemonOutbreaksConfig {
         OUTBREAK_SPAWN_COUNT = builder.comment("Amount of outbreaks that spawn when the timer runs out, 0 spawns nothing").define("outbreak_spawn_count", 3);
         SEND_PORTAL_SPAWN_MESSAGE = builder.comment("Whether or not a message should be sent when an outbreak spawns, finishes and gets removed").define("send_outbreak_portal_spawn_message", true);
         SPAWN_PORTAL_PARTICLES = builder.comment("Turn particles on or off for the outbreak portal, this will make it easier to find them").define("spawn_portal_particles", false);
+        BIOME_SPECIFIC_SPAWNS = builder.comment("Whether or not the outbreaks should spawn in predetermined biomes").define("biome_specific_spawns", false);
+        BIOME_SPECIFIC_SPAWNS_DEBUG = builder.comment("A message that tells you what biome an outbreak is currently spawning in with their pokemon and if you expected this").define("biome_specific_spawns_debug", false);
 
+        MIN_SPAWN_RADIUS = builder.comment("Minumum spawn radius the portal can spawn around the player, minumum is 16, maximum is 48").define("min_spawn_radius", 32);
+        MAX_SPAWN_RADIUS = builder.comment("Maximum spawn radius the portal can spawn around the player, minumum is 49 maximum is 112").define("max_spaw_radius", 64);
     }
 }
--- src/main/java/com/scouter/cobbleoutbreaks/data/OutbreaksJsonDataManager.java
+++ src/main/java/com/scouter/cobbleoutbreaks/data/OutbreaksJsonDataManager.java
@@ -5,17 +5,18 @@ import com.google.gson.JsonElement;
 import com.mojang.logging.LogUtils;
 import com.mojang.serialization.JsonOps;
 import com.scouter.cobbleoutbreaks.entity.OutbreakPortal;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.scouter.cobbleoutbreaks.CobblemonOutbreaks.prefix;
 
@@ -27,7 +28,9 @@ public class OutbreaksJsonDataManager extends SimpleJsonResourceReloadListener {
 
 
     protected static Map<ResourceLocation, OutbreakPortal> data = new HashMap<>();
+    protected static Map<ResourceKey<Biome>, Map<ResourceLocation, OutbreakPortal>> biomeData = new HashMap<>();
     protected static List<ResourceLocation> resourceLocationList = new ArrayList<>();
+    protected static Map<ResourceKey<Biome>, List<ResourceLocation>> resourceLocationMap = new HashMap();
     private final String folderName;
     public OutbreaksJsonDataManager()
     {
@@ -40,23 +43,47 @@ public class OutbreaksJsonDataManager extends SimpleJsonResourceReloadListener {
         super(gson, folderName);
         this.folderName = folderName;
     }
-    public static Map<ResourceLocation, OutbreakPortal> getData()
-    {
+
+
+    public static Map<ResourceLocation, OutbreakPortal> getData() {
         return data;
     }
 
-    public static OutbreakPortal getPortalFromRl(ResourceLocation resourceLocation, OutbreakPortal outbreakPortal)
-    {
+    public static Map<ResourceKey<Biome>, Map<ResourceLocation, OutbreakPortal>> getBiomeData() {
+        return biomeData;
+    }
+
+    public static OutbreakPortal getPortalFromRl(ResourceLocation resourceLocation, OutbreakPortal outbreakPortal) {
         return data.getOrDefault(resourceLocation, outbreakPortal);
     }
 
-    public static Map<ResourceLocation, OutbreakPortal> getRandomPortal(Level level)
-    {
-        Map<ResourceLocation, OutbreakPortal> map = new HashMap();
-        ResourceLocation rl = resourceLocationList.get(level.random.nextInt(0,resourceLocationList.size()));
+    public static Map<ResourceLocation, OutbreakPortal> getRandomPortalFromBiome(Level level, ResourceKey<Biome> biome) {
+        Map<ResourceLocation, OutbreakPortal> map = new HashMap<>();
+        ResourceLocation rl = getRandomResourceLocationFromBiome(level, biome);
+        Map<ResourceLocation, OutbreakPortal> outbreakPortalMap = biomeData.getOrDefault(biome, new HashMap<>());
+        OutbreakPortal outbreakPortal = outbreakPortalMap.getOrDefault(rl, getPortalFromRl(rl, null));
+        map.put(rl, outbreakPortal);
+        return map;
+    }
+
+    public static Map<ResourceLocation, OutbreakPortal> getRandomPortal(Level level) {
+        Map<ResourceLocation, OutbreakPortal> map = new HashMap<>();
+        ResourceLocation rl = getRandomResourceLocation(level);
         OutbreakPortal outbreakPortal = data.get(rl);
         map.put(rl, outbreakPortal);
         return map;
+    }
+
+    private static ResourceLocation getRandomResourceLocationFromBiome(Level level, ResourceKey<Biome> biome) {
+        List<ResourceLocation> resourceLocations = resourceLocationMap.getOrDefault(biome, Collections.emptyList());
+        if (resourceLocations.isEmpty()) {
+            return getRandomResourceLocation(level);
+        }
+        return resourceLocations.get(level.random.nextInt(resourceLocations.size()));
+    }
+
+    private static ResourceLocation getRandomResourceLocation(Level level) {
+        return resourceLocationList.get(level.random.nextInt(resourceLocationList.size()));
     }
 
 
@@ -64,28 +91,43 @@ public class OutbreaksJsonDataManager extends SimpleJsonResourceReloadListener {
     protected void apply(Map<ResourceLocation, JsonElement> jsons, ResourceManager pResourceManager, ProfilerFiller pProfiler) {
         LOGGER.info("Beginning loading of data for data loader: {}", this.folderName);
         Map<ResourceLocation, OutbreakPortal> newMap = new HashMap<>();
+        Map<ResourceKey<Biome>, List<ResourceLocation>> resourceLocationBiomeMap = new HashMap<>();
+        Map<ResourceKey<Biome>, Map<ResourceLocation, OutbreakPortal>> newBiomeData = new HashMap<>();
         List<ResourceLocation> newResourceLocationList = new ArrayList<>();
         data.clear();
         resourceLocationList.clear();
-        for (Map.Entry<ResourceLocation, JsonElement> entry : jsons.entrySet())
-        {
+
+        for (Map.Entry<ResourceLocation, JsonElement> entry : jsons.entrySet()) {
             ResourceLocation key = entry.getKey();
             JsonElement element = entry.getValue();
 
             // if we fail to parse json, log an error and continue
-            // if we succeeded, add the resulting T to the ma
+            // if we succeeded, add the resulting T to the map
             OutbreakPortal.CODEC.decode(JsonOps.INSTANCE, element)
                     .get()
                     .ifLeft(result -> {
-                        newMap.put(key, result.getFirst());
-                        resourceLocationList.add(key);
+                        OutbreakPortal portal = result.getFirst();
+                        newMap.put(key, portal);
+                        List<ResourceLocation> spawnBiome = portal.getSpawnBiome();
+                        spawnBiome.forEach(biome -> {
+                            ResourceKey<Biome> biomeResourceKey = ForgeRegistries.BIOMES.getHolder(biome).get().unwrapKey().get();
+                            List<ResourceLocation>  resourceLocations = resourceLocationBiomeMap.getOrDefault(biomeResourceKey, new ArrayList<>());
+                            resourceLocations.add(key);
+                            resourceLocationBiomeMap.put(biomeResourceKey, resourceLocations);
+                            Map<ResourceLocation, OutbreakPortal> mapToPut = newBiomeData.computeIfAbsent(biomeResourceKey, k -> new HashMap<>());
+                            mapToPut.put(key, portal);
+                            newBiomeData.put(biomeResourceKey, mapToPut);
+                        });
+                        newResourceLocationList.add(key);
                     })
-                    .ifRight(partial -> LOGGER.error("Failed to parse data json for {} due to: {}", key, partial.message()));
+                    .ifRight(partial -> LOGGER.error("Failed to parse data json for {} due to: {}", entry.getKey(), partial.message()));
         }
 
-
-        this.resourceLocationList.addAll(newResourceLocationList);
+        this.resourceLocationList = newResourceLocationList;
         this.data = newMap;
+        this.biomeData = newBiomeData;
+
+        this.resourceLocationMap = resourceLocationBiomeMap;
         LOGGER.info("Data loader for {} loaded {} jsons", this.folderName, this.data.size());
     }
 }
